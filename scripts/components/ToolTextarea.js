9350f4144d41e99bd2a20725b0e4f3c142a7a86f
Yu Nozato <nozato.yu@hamee.co.jp>
2019-09-16 23:05:34 +0900

    initial commit

---
+++ scripts/components/ToolTextarea.js
@@ -0,0 +1,36 @@
+import html from '../html.js';
+
+/**
+ * @param {object} props
+ * @param {string} props.value
+ * @param {Function} props.onChange
+ * @param {boolean|undefined} props.readonly
+ * @param {string|undefined} props.placeholder
+ */
+const ToolTextarea = ({ value, onChange, readonly, placeholder }) => {
+  const onChangeHandler = event => {
+    if (onChange && readonly !== true) {
+      onChange(event.currentTarget.value);
+    }
+  };
+
+  return !readonly
+    ? html`
+        <textarea
+          value=${value}
+          onChange=${onChangeHandler}
+          placeholder=${placeholder ? placeholder : ''}
+          spellCheck="false"
+        />
+      `
+    : html`
+        <textarea
+          value=${value}
+          readOnly=${readonly}
+          placeholder=${placeholder ? placeholder : ''}
+          spellCheck="false"
+        />
+      `;
+};
+
+export default ToolTextarea;
---
+++ scripts/html.js
@@ -0,0 +1,6 @@
+import htm from 'https://unpkg.com/htm?module'
+import React from 'https://dev.jspm.io/react';
+
+const html = htm.bind(React.createElement);
+
+export default html;
