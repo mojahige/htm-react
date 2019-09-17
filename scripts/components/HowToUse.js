5d1ba6f31ec5322ad172f366919732d98a2a6576
mojahige <mojahige@gmail.com>
2019-09-17 12:01:52 +0900

    update: 説明アップデート

--- scripts/components/HowToUse.js
+++ scripts/components/HowToUse.js
@@ -4,18 +4,21 @@ import Modal from '../containers/Modal.js';
 const HowToUse = () => {
   const Description = () => html`
     <ol className="HowToUse">
-      <li key="a">
+      <li>
         上のテキストエリアに文字を入力するよ。<br />
         <small>改行を1つの区切りとして扱うよ</small>
       </li>
-      <li key="b">
+      <li>
         下に入力した文字が li で囲まれて表示されるよ。
+      </li>
+      <li>
+        下に表示された文字をコピペして使うよ。
       </li>
     </ol>
   `;
 
   return html`
-    <${Modal} openLabel="How to use?">
+    <${Modal} openLabel="How to use? 👊">
       <${Description} />
     <//>
   `;
